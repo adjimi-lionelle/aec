a795e7eb2a13d7eb083abc38d862aa2660640e00
Urelle <urellefosso123@gmail.com>
2025-03-14 14:29:58 +0100

    initialise le projet

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,34 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/t', function () {
+    return view('layouts/index');
+});
+
+Route::get('/r', function () {
+    return view('layouts/author');
+});
+
+Route::get('/a', function () {
+    return view('layouts/about');
+});
+
+Route::get('/c', function () {
+    return view('layouts/categorie');
+});
+
+Route::get('/o', function () {
+    return view('layouts/contact');
+});
+
+Route::get('/d', function () {
+    return view('layouts/detail-blog');
+});
+
+Route::get('/f', function () {
+    return view('layouts/connecter');
+});
+
+
+
